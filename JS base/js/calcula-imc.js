fb6b22f1dead1e67e297a8406023b003c87a633b
Bruno Couto <brunnohcouto@gmail.com>
2018-10-07 06:19:30 -0300

    Fase 6 do curso base completada

---
+++ JS base/js/calcula-imc.js
@@ -0,0 +1,69 @@
+// NOTE: Seleciona titulo e altera
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+// NOTE: Seleciona todos pacientes afim de criar um array para percorrer.
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
+
+    if (!pesoEhValido) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (!alturaEhValida) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+
+/*
+##################
+### FUNCTIONS ### 
+##################
+*/
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+// NOTE: Verifica se o peso eh valido
+// TRUE = Peso valido || FALSE = Peso invalido
+function validaPeso(peso){
+    if(peso > 0 && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+// NOTE: Verifica se a altura eh valida
+// TRUE = altura valida && FALSE = altura invalida
+function validaAltura(altura){
+    if(altura > 0 && altura < 4.0){
+        return true;
+    }else{
+        return false;
+    }
+}
---
+++ JS base/js/form.js
@@ -0,0 +1,96 @@
+// NOTE: Aguarda acao do botao após click
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+    var paciente = obtemPacienteDoFormulario(form);
+    var pacienteTr = montaTr(paciente);
+    var erro = validaPaciente(paciente);
+    // NOTE: Verifica se há algum erro
+    // Se erro maior que 0 ira ser exibido e saira imediatamente da funcao
+    if(erro.length > 0){
+        exibeErro(erro);
+        return;
+    }
+    if(!validaPaciente(paciente)){
+        console.log("Paciente invalido");
+        // NOTE: Usando "return" sem um dado, sai imediatamente da funcao caso o paciente seja
+        return;
+    }
+
+    // NOTE: Adicionando paciente na tabela
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+    form.reset();
+    var mensagemErro = document.querySelector("#mensagensErro");
+    mensagemErro.innerHTML = "";
+});
+
+/*
+##################
+### FUNCTIONS ### 
+##################
+*/
+function obtemPacienteDoFormulario(form) {
+    // NOTE: Recebe um array de dados e cria um objeto paciente
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+    return paciente;
+}
+
+// NOTE: Monta a linha da tabela
+function montaTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+// NOTE: Monta os dados da tabela
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+    return td;
+}
+
+function validaPaciente(paciente){
+    var erros = [];
+    if(!validaPeso(paciente.peso)){
+        erros.push("Peso invalido");
+    }
+    if(!validaAltura(paciente.altura)){
+        erros.push("Altura invalida!");
+    }
+    if(paciente.nome.length == 0){
+        erros.push("Nome não pode ser nulo");
+    }
+    if(paciente.gordura.length == 0){
+        erros.push("Gordura não pode ser em branco.");
+    }
+    console.log(erros);
+    return erros;
+}
+
+function exibeErro(erro){
+    var ul = document.querySelector("#mensagensErro");
+    // NOTE: Limpa o conteudo interno da UL
+    ul.innerHTML = "";
+    erro.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
